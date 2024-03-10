ad5ffdefe99f8c1d4b089b08c58e9a5eaa101653
Keir Finlow-Bates <keir@chainfrog.com>
2024-03-10 13:44:16 +0200

    Pre and post fix

--- vanity-address.py
+++ vanity-address.py
@@ -2,21 +2,29 @@ import sys
 import os
 from eth_account import Account
 
-if len(sys.argv) < 2:
-    print("Vanity prefix not provided")
+if len(sys.argv) < 3:
+    print("Vanity suffix and prefix not provided")
     sys.exit()
 
-vanity = sys.argv[1]
+vanityPrefix = sys.argv[1]
+
+vanitySuffix = sys.argv[2]
 
 try:
-    int(vanity, 16)
+    int(vanityPrefix, 16)
 except:
     print("Vanity prefix must be a hex string")
     sys.exit()
+    
+try:
+    int(vanitySuffix, 16)
+except:
+    print("Vanity suffix must be a hex string")
+    sys.exit()
 
-print("Searching for address starting with 0x" + vanity)
-print("This is length " + str(len(vanity)))
-print("Expected number of tries: " + str(16**len(vanity)))
+print("Searching for address starting with 0x" + vanityPrefix + " and ending with " + vanitySuffix)
+print("This is length " + str(len(vanitySuffix) + len(vanityPrefix)))
+print("Expected number of tries: " + str(16**len(vanitySuffix + vanityPrefix)))
 count = 0
 
 while True:
@@ -26,6 +34,6 @@ while True:
     privkey = os.urandom(32)
     acct = Account.privateKeyToAccount(privkey)
     acct_addr = acct.address
-    if acct_addr[2:len(vanity)+2].lower() == vanity:
+    if acct_addr[2:len(vanityPrefix)+2].lower() == vanityPrefix and acct_addr[-1*len(vanitySuffix):].lower() == vanitySuffix:
         print("Private key: " + str(privkey.hex()))
-        print("Deployment address: " + acct_addr)
+        print("Account address: " + acct_addr)
